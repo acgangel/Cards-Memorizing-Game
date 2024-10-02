45c34f6be5bebabccd4ba8f3905239a4dcd73558
acgangel <acgangel650002@gmail.com>
2024-10-03 00:09:05 +0800

    feat: view中新增數字轉換成AJOK的函式

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // 將變數首字母大寫以表示資料不會變動
 const Symbols = [
-  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // 黑桃
-  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
-  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
-  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
+  './assets/club.png', // 黑桃
+  './assets/heart.png', // 愛心
+  './assets/diamonds.png', // 方塊
+  './assets/club.png' // 梅花
 ]
 // 0 - 12：黑桃 1 - 13
 // 13 - 25：愛心 1 - 13
@@ -21,10 +21,25 @@ const Symbols = [
 // }
 
 const view = {
+  // 數字轉換成AJOK
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
   // 負責生成卡片內容，包括花色和數字
   getCardElement(index) {
-    const number = (index % 13) + 1
-    const symbol = Symbols[Math.floor(index / 13)]
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)] //向下取整到最接近的整數
     return `
       <div class="card">
         <p>${number}</p>
