0379ef87f45366b7766430b22bdd348da490bd6e
acgangel <acgangel650002@gmail.com>
2024-10-27 23:58:08 +0800

    feat: 把 setTimeout() 裡的動作獨立成一個 resetCards 來管理

--- main.js
+++ main.js
@@ -124,16 +124,19 @@ const controller = {
         } else {
           // 配對失敗
           this.currentState = GAME_STATE.CardsMatchFailed
-          setTimeout(() => {
-            view.flipCards(...model.revealedCards)
-            model.revealedCards = []
-            this.currentState = GAME_STATE.FirstCardAwaits
-          }, 1000)
+          setTimeout(this.resetCards, 1000)
         }
         break
     }
     console.log('this.currentState', this.currentState)
     console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+  },
+
+  //把 setTimeout() 裡的動作獨立成一個 resetCards 來管理
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    this.currentState = GAME_STATE.FirstCardAwaits
   }
 }
 
